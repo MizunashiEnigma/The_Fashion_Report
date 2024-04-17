34114562b602ea09f1ca653adbb247428bb71416
MizunashiEnigma <123388502+MizunashiEnigma@users.noreply.github.com>
2024-04-17 12:38:07 +0100

    adjust fashionService

--- src/service/fashionservice.service.ts
+++ src/service/fashionservice.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
-import {Fashionresponse} from ./src/fashionresponse
+import { FashionInterface } from '../interfaces/fashion-interface';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError, tap } from "rxjs";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FashionserviceService {
 
-  constructor() { }
+  constructor(private _https:HttpClient) { }
+  private _APIURL = 'https://ffxivcollect.com/api/fashions/1'
 }
