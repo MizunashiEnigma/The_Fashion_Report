a8069aa09a51511f12430369cf85a4eb6ce6fca7
MizunashiEnigma <123388502+MizunashiEnigma@users.noreply.github.com>
2024-04-17 12:08:34 +0100

    added the interface to accept the JSON

---
+++ src/app/fashionresponse.ts
@@ -0,0 +1,11 @@
+export interface CollectInterface{
+    name:string;
+    image:string;
+    patch:string;
+}
+export interface Sources{
+    type:string;
+    text:string;
+    related_type:string | any
+    related_id:string | any
+}
